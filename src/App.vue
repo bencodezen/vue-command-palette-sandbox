c02c6e25ba61f11a9a8fc0b8e3d95383cf02cadb
Ben Hong <ben@bencodezen.io>
2022-04-19 09:28:13 -0700

    feature: add ability to toggle a simple command palette component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, watch } from 'vue'
+import { useMagicKeys } from '@vueuse/core'
+import CommandPalette from './components/CommandPalette.vue'
+
+const displayCommandPalette = ref(false)
+const keys = useMagicKeys()
+
+watch(keys.Cmd_K, isPressed => {
+  if (isPressed) {
+    displayCommandPalette.value = !displayCommandPalette.value
+  }
+})
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <CommandPalette v-if="displayCommandPalette" />
 </template>
 
 <style>
---
+++ src/components/CommandPalette.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="command-palette">
+    <h1>My Command Palette</h1>
+  </div>
+</template>
+
+<style></style>
